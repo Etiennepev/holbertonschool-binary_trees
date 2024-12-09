fe27576d6ae9377880af570f459f1b22f87ec938
Etiennepev <etienneloichot@gmail.com>
2024-12-09 14:04:56 +0100

    Pushing the file binary tree insert right

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - Creates a new binary tree node
+ *
+ * @parent: Pointer to the parent node of the node to create
+ * @value: Value to assign to the new node
+ * Return: a pointer to the uncle node
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+	}
+	else
+		new_node->right = NULL;
+	parent->right = new_node;
+
+	return (new_node);
+}
